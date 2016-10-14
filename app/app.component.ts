bbc1be8febb4ac2bd1ac904434416f7d8fd5aa22
Randy Sagum <randy.sagum@yahoo.com>
2016-10-14 14:15:23 -0700

    added hellow world prop.

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>{{helloworld}}</h1>'
+})
+export class AppComponent {
+
+  helloworld : "Hello World!"
+
+
+
+ }
