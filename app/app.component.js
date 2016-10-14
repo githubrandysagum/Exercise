65b942a897b14efb42f6090a7669b32ceb4971d3
Randy Sagum <randy.sagum@yahoo.com>
2016-10-14 15:26:40 -0700

    add title and myhero prop ro AppComponent

--- app/app.component.js
+++ app/app.component.js
@@ -11,12 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.hell = "Hello world!";
+        this.title = 'Tour of Heroes';
+        this.myHero = 'Windstorm';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>{{hell}}</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My favorite hero is: {{myHero}}</h2>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
