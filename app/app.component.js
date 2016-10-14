e53682a39a6567823c58e7a5a6b514434a909129
Randy Sagum <randy.sagum@yahoo.com>
2016-10-14 16:05:09 -0700

    Just retain the appcomponent data

--- app/app.component.js
+++ app/app.component.js
@@ -9,21 +9,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var ClickMeComponent = (function () {
-    function ClickMeComponent() {
-        this.clickMessage = '';
+var hero_1 = require('./hero');
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.heroes = [
+            new hero_1.Hero(1, 'Windstorm'),
+            new hero_1.Hero(13, 'Bombasto'),
+            new hero_1.Hero(15, 'Magneta'),
+            new hero_1.Hero(20, 'Tornado')
+        ];
+        this.myHero = this.heroes[0];
     }
-    ClickMeComponent.prototype.onClickMe = function () {
-        this.clickMessage = 'You are my hero!';
-    };
-    ClickMeComponent = __decorate([
+    AppComponent = __decorate([
         core_1.Component({
-            selector: 'click-me',
-            template: "\n    <button (click)=\"onClickMe()\">Click me!</button>\n    {{clickMessage}}"
+            selector: 'my-app',
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My favorite hero is: {{myHero.name}}</h2>\n    <p>Heroes</p>\n    <ul>\n      <li *ngFor = \"let hero of heroes\">\n        {{hero.name}}\n      </li>\n    </ul>\n    <p *ngIf=\"heroes.length > 3\">There are many heroes!</p>\n\n    "
         }), 
         __metadata('design:paramtypes', [])
-    ], ClickMeComponent);
-    return ClickMeComponent;
+    ], AppComponent);
+    return AppComponent;
 }());
-exports.ClickMeComponent = ClickMeComponent;
+exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
